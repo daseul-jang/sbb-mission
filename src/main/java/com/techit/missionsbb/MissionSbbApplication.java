5bbd91098a94ef8ce933d98c84e7631cd28d0b2a
daseul-jang <devzzang94@gmail.com>
2023-11-21 09:29:55 +0900

    프로젝트 생성

---
+++ src/main/java/com/techit/missionsbb/MissionSbbApplication.java
@@ -0,0 +1,13 @@
+package com.techit.missionsbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionSbbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissionSbbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techit/missionsbb/MissionSbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techit.missionsbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionSbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
